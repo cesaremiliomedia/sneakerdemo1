44550ee960e1371dd5fe4837a59e1e9547d089dc
Vanessa Mendez <vanessamendez@vanessas-air.attlocal.net>
2020-08-05 06:37:16 -0500

    h1fix

--- src/components/SneakerStore/productCarusel.js
+++ src/components/SneakerStore/productCarusel.js
@@ -1,15 +1,15 @@
-import React, { useState, useEffect } from 'react'
-import Shoes from './Shoes'
-import Accessories from './Accessories'
-import Bags from './Bags'
+import React, { useState, useEffect } from "react"
+import Shoes from "./Shoes"
+import Accessories from "./Accessories"
+import Bags from "./Bags"
 export default function App() {
   return (
-    <div className="" style={{ background: 'white' }}>
+    <div className="" style={{ background: "white" }}>
       <div
         id="carouselExampleIndicators"
         className="carousel slide"
         data-ride="carousel"
-        style={{ minHeight: '400px' }}
+        style={{ minHeight: "400px" }}
       >
         <ol className="carousel-indicators">
           <li data-target="slide-1" data-slide-to="0" className="active"></li>
@@ -17,29 +17,29 @@ export default function App() {
           <li data-target="slide-3" data-slide-to="2"></li>
         </ol>
         <div style={{}} className="carousel-inner w-100">
-          <div className="carousel-item active">
-            <h1>Footwear</h1>
+          <div className=" mt-3 carousel-item active">
+            <h1 className="h1bs">Footwear</h1>
 
             <Shoes></Shoes>
           </div>
-          <div id="slide-2" className="carousel-item">
-            <h1>Bags</h1>
+          <div className="mt-3 carousel-item">
+            <h1 className="h1bs">Bags</h1>
             <Bags></Bags>
           </div>
-          <div className="carousel-item">
-            <h1>accessories</h1>
+          <div className="mt-3 carousel-item">
+            <h1 className="h1bs">accessories</h1>
             <Accessories></Accessories>
           </div>
         </div>
         <a
-          style={{ color: 'red' }}
+          style={{ color: "red" }}
           className="carousel-control-prev"
           href="#carouselExampleIndicators"
           role="button"
           data-slide="prev"
         >
           <i
-            style={{ color: 'black' }}
+            style={{ color: "black" }}
             class="fa fa-chevron-left"
             aria-hidden="true"
           ></i>
@@ -47,7 +47,7 @@ export default function App() {
           <span className="sr-only">Previous</span>
         </a>
         <a
-          style={{ color: 'black' }}
+          style={{ color: "black" }}
           className="carousel-control-next"
           href="#carouselExampleIndicators"
           role="button"
